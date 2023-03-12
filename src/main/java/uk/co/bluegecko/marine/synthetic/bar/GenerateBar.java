9d7c1ee62b3afaf06db615ef0a779c236e6b2a0d
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-03-12 21:31:16 +0000

    build scripts - first pass

---
+++ src/main/java/uk/co/bluegecko/marine/synthetic/bar/GenerateBar.java
@@ -0,0 +1,7 @@
+package uk.co.bluegecko.marine.synthetic.bar;
+
+public class GenerateBar {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
---
+++ src/main/java/uk/co/bluegecko/marine/synthetic/foo/GenerateFoo.java
@@ -0,0 +1,7 @@
+package uk.co.bluegecko.marine.synthetic.foo;
+
+public class GenerateFoo {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
