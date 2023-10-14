8957f8c73ff874ec0458e7f36916022b3e4903fe
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-10-14 12:56:09 +0100

    fix threading on synthetic publisher

--- src/main/java/uk/co/bluegecko/marine/synthetic/ais/raw/AisFeed.java
+++ src/main/java/uk/co/bluegecko/marine/synthetic/ais/raw/AisFeed.java
@@ -19,7 +19,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.command.annotation.Option;
@@ -31,66 +33,72 @@ import uk.co.bluegecko.marine.shared.configuration.SchedulerConfiguration;
 @Component
 @Value
 @Slf4j
-@Import(SchedulerConfiguration.class)
+@Import({SchedulerConfiguration.class})
 public class AisFeed {
 
 	TaskScheduler scheduler;
+	TaskExecutor executor;
 	Lorem lorem;
 
 	@Command(description = "Publish synthetic AIS feed.")
 	public void publish(
 			@Option(shortNames = 'p', defaultValue = "10000", description = "Port to publish feed on.") int port,
 			@Option(shortNames = 'd', defaultValue = "1000", description = "Delay between messages in ms.") long delay) {
-		try {
-			startServer(port, delay);
-		} catch (IOException ex) {
-			log.error("Unable to create server on port {} because of {}", port, ex.getMessage());
-		} catch (InterruptedException ex) {
-			stop();
-		}
+		startServer(port, Duration.ofMillis(delay));
 	}
 
 	@Command(description = "Stop publishing feed", interactionMode = InteractionMode.INTERACTIVE)
 	public void stop() {
-		log.info("Stopping publisher");
-		if (scheduler instanceof ThreadPoolTaskScheduler poolTaskScheduler) {
-			poolTaskScheduler.shutdown();
+		if (isRunning()) {
+			log.info("Stopping publisher");
+			if (getScheduler() instanceof ThreadPoolTaskScheduler taskScheduler) {
+				taskScheduler.shutdown();
+			}
+			if (getExecutor() instanceof ThreadPoolTaskExecutor taskExecutor) {
+				taskExecutor.shutdown();
+			}
 		}
 	}
 
-	private void startServer(int port, long delay) throws IOException, InterruptedException {
+	private void startServer(int port, Duration delay) {
 		if (isRunning()) {
-			SelectorProvider provider = SelectorProvider.provider();
-			Selector selector = provider.openSelector();
-			ServerSocketChannel server = provider.openServerSocketChannel();
-			server.bind(new InetSocketAddress("127.0.0.1", port));
-			server.configureBlocking(false);
-			server.register(selector, SelectionKey.OP_ACCEPT);
-			log.info("Publishing AIS Feed on port {} every {} ns", port, delay);
-			while (isRunning()) {
-				selector.select(key -> {
-					if (key.isAcceptable()) {
-						startPublishing(selector, server, delay);
+			getExecutor().execute(() -> {
+				try {
+					SelectorProvider provider = SelectorProvider.provider();
+					Selector selector = provider.openSelector();
+					try (ServerSocketChannel server = provider.openServerSocketChannel()) {
+						server.bind(new InetSocketAddress("127.0.0.1", port));
+						server.configureBlocking(false);
+						server.register(selector, SelectionKey.OP_ACCEPT);
+						log.info("Publishing AIS Feed on port {} every {} ns", port, delay);
+						while (isRunning()) {
+							selector.select(key -> {
+								if (key.isAcceptable()) {
+									startPublishing(selector, server, delay);
+								}
+							});
+						}
 					}
-				});
-			}
+				} catch (IOException ex) {
+					log.error("Unable to create server on port {} due to {}", port, ex.getMessage());
+				}
+			});
 		} else {
 			log.info("Publisher already stopped");
 		}
 	}
 
-	private void startPublishing(Selector selector, ServerSocketChannel server, long delay) {
+	private void startPublishing(Selector selector, ServerSocketChannel server, Duration delay) {
 		log.info("Incoming connection...");
 		try {
 			SocketChannel client = server.accept();
 			log.info("Connected to {} :: {}", client.getLocalAddress(), client.getRemoteAddress());
 			client.configureBlocking(false);
 			SelectionKey key = client.register(selector, SelectionKey.OP_WRITE);
-			ScheduledFuture<?> future = scheduler.scheduleAtFixedRate(() -> writeMessage(key),
-					Duration.ofMillis(delay));
+			ScheduledFuture<?> future = getScheduler().scheduleAtFixedRate(() -> writeMessage(key), delay);
 			key.attach(future);
 		} catch (IOException ex) {
-			log.error("Unable to connect to client because {}", ex.getMessage());
+			log.error("Unable to connect to client: {}", ex.getMessage());
 		}
 	}
 
@@ -101,7 +109,7 @@ public class AisFeed {
 				((WritableByteChannel) key.channel()).write(buffer);
 			}
 		} catch (IOException ex) {
-			log.warn("Unable to write message to client because of {}", ex.getMessage());
+			log.debug("Client disconnected, stopping writing.");
 			((ScheduledFuture<?>) key.attachment()).cancel(true);
 			try {
 				key.channel().close();
