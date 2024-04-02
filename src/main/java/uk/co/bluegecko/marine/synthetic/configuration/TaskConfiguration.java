c5661d5cf50f8ef16f5e0e4136885a3a89a0db82
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-04-02 19:13:58 +0100

    clean up ais loader and integration tests

--- src/main/java/uk/co/bluegecko/marine/synthetic/configuration/TaskConfiguration.java
+++ src/main/java/uk/co/bluegecko/marine/synthetic/configuration/TaskConfiguration.java
@@ -1,15 +1,21 @@
 package uk.co.bluegecko.marine.synthetic.configuration;
 
+import org.springframework.boot.task.TaskExecutorCustomizer;
 import org.springframework.boot.task.TaskSchedulerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SchedulerExtraConfiguration {
+public class TaskConfiguration {
 
 	@Bean
 	public TaskSchedulerCustomizer taskSchedulerCustomizer() {
 		return taskScheduler -> taskScheduler.setDaemon(true);
+	}
+
+	@Bean
+	public TaskExecutorCustomizer taskExecutorCustomizer() {
+		return taskExecutor -> taskExecutor.setDaemon(true);
 	}
 
 }
