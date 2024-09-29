1fea6ac8726bb6f133dc687be11eb5cf306af208
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-09-29 18:03:04 +0100

    upgrade java, gradle and library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation(project(":shared"))
     implementation(project(":wire"))
-    implementation("org.springframework.shell:spring-shell-starter:3.1.4")
+    implementation("org.springframework.shell:spring-shell-starter:3.3.3")
     implementation("com.thedeanda:lorem:2.2")
     testImplementation(project(":test"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
